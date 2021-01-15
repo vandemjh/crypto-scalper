6078ed0f18cd028b9d7eebb5232fe704f0375421
vandemjh <jack@jackv.dev>
2021-01-15 13:32:47 -0500

    Testing stuff out

--- scalp.py
+++ scalp.py
@@ -1,9 +1,29 @@
 import requests
 import os
+import json
 from dotenv import load_dotenv
+from binance.client import Client
+from binance.enums import *
+
+# Percent of portfolio to trade
+IN_PLAY_PERCENT = 1
+# Symbol to scalp
+SYMBOL = "BTCUSDT"
+
 
 load_dotenv()
-KEY = os.getenv("KEY")
-SECRET = os.getenv("SECRET")
+api_key = os.getenv("KEY")
+api_secret = os.getenv("SECRET")
+client = Client(api_key, api_secret, tld="us")
+api_key = api_secret = ""
 
-print(requests.get("https://binance.us/api/v3/time").text)
+USDTBalance = (client.get_asset_balance(asset="USDT"))["free"]
+USDTBalance = float(USDTBalance) * (IN_PLAY_PERCENT / 100)
+
+# avg_price = client.get_avg_price(symbol=SYMBOL)
+
+# print(avg_price)
+
+
+# order = client.order_limit_buy(symbol=SYMBOL, quantity=100, price="0.00001")
+# order = client.order_limit_sell(symbol=SYMBOL, quantity=100, price="0.00001")
