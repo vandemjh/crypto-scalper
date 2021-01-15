d30123bdc1bdb21f1fc2bd3772db124200983f80
vandemjh <jack@jackv.dev>
2021-01-15 15:38:07 -0500

    Added colors to print

---
+++ colors.py
@@ -0,0 +1,14 @@
+# Adapted from Blender source code
+class colors:
+    RED = ERROR = "\u001b[31m"
+    GREEN = FILLED = "\u001b[32m"
+    YELLOW = PLACED = "\u001b[33m"
+    BLUE = "\u001b[34m"
+    MAGENTA = SELL = "\u001b[35m"
+    CYAN = BUY = "\u001b[36m"
+    WHITE = "\u001b[37m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    BOLD = INFO = "\033[1m"
+    UNDERLINE = "\033[4m"
+    END = "\033[0m"
