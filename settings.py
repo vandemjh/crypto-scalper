28246fdae6af13635b6bc2bcb2a2361974880214
vandemjh <jack@jackv.dev>
2021-01-16 17:18:49 -0500

    Extracted settings file

---
+++ settings.py
@@ -0,0 +1,8 @@
+# Percent of portfolio to trade
+IN_PLAY_PERCENT = 1
+# Symbol to scalp
+SYMBOL = "BTCUSDT"
+DEBUG = True
+SCALP_PERCENT = 1
+# Export trades to file
+OUTPUT_FILE = "output/orders.json"
