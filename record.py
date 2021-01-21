65d3b09769b680bdf7b71426ff26988a8240255e
vandemjh <jack@jackv.dev>
2021-01-20 22:49:50 -0500

    added recording file for recording price for testing

---
+++ record.py
@@ -0,0 +1,31 @@
+import os
+import json
+import threading
+import time
+from binance.client import Client as BinanceClient
+from dotenv import load_dotenv
+from binance.enums import *
+from util.client import Client
+
+load_dotenv()
+api_key = os.getenv("KEY")
+api_secret = os.getenv("SECRET")
+Client.setClient(BinanceClient(api_key, api_secret, tld="us"))
+api_key = api_secret = None
+
+ORDER_HISTORY = []
+OUTPUT_FILE = "out.json"
+
+def writeToFile() -> None:
+    with open(OUTPUT_FILE, "w+") as f:
+        f.write(json.dumps(ORDER_HISTORY))
+
+
+count = 0
+while True:
+    count = count + 1
+    time.sleep(1)
+    ORDER_HISTORY.append(Client.latestPrice)
+    if count > 600:
+        threading.Thread(target=writeToFile).start()
+        count = 0
