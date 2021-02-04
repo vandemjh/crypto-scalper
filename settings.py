af683c793ae42a39be7bfc52f6a81c74d3f18b01
vandemjh <jack@jackv.dev>
2021-02-04 14:03:27 -0500

    new settings options

--- settings.py
+++ settings.py
@@ -1,8 +1,17 @@
 # Percent of portfolio to trade
-IN_PLAY_PERCENT = 95
+# Set below 100% to avoid rounding errors
+IN_PLAY_PERCENT = 5
 # Symbol to scalp
 SYMBOL = "DOGEUSDT"
-DEBUG = False
-SCALP_PERCENT = 3
+# Percent to scalp
+SCALP_PERCENT = 0.5
+# Number of parallel trades
+NUMBER_OF_TRADES = 3
+# Increase to avoid hitting the API so many times
+SLEEP_MULTIPLIER = 1
+
+# Don't change these
 # Export trades to file in json
 OUTPUT_FILE = "output/orders"
+ORDER_HISTORY = []
+DEBUG = False
