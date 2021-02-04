ede827faba04c8f084982e5870e79c6e943bc0be
vandemjh <jack@jackv.dev>
2021-02-03 20:57:56 -0500

    I will never code in python again

--- model/exchange.py
+++ model/exchange.py
@@ -1,4 +1,39 @@
+from settings import IN_PLAY_PERCENT, SYMBOL
+from util.client import Client
+
+
 class ExchangeInformation:
-    def ___init___(self):
-        from util.util import Util
-        self = Util.getExchangeInformation()
+    baseAsset: str = None
+    baseAssetPrecision: int = None
+    quoteAsset: str = None
+    quoteAssetPrecision: int = None
+    minPrice: float = None
+    maxPrice: float = None
+    tickSize: float = None
+    stepSize: float = None
+    balance: float = None
+
+    @staticmethod
+    def init():
+        exchangeInfo: dict = Client.getExchangeInformation()
+        symbols = exchangeInfo["symbols"]
+        filters: list = []
+        for symbol in symbols:
+            if symbol["symbol"] == SYMBOL:
+                ExchangeInformation.baseAsset = symbol["baseAsset"]
+                ExchangeInformation.baseAssetPrecision = symbol["baseAssetPrecision"]
+                ExchangeInformation.quoteAsset = symbol["quoteAsset"]
+                ExchangeInformation.quoteAssetPrecision = symbol["quotePrecision"]
+                filters = symbol["filters"]
+        for filter in filters:
+            if filter["filterType"] == "PRICE_FILTER":
+                ExchangeInformation.minPrice = float(filter["minPrice"])
+                ExchangeInformation.maxPrice = float(filter["maxPrice"])
+                ExchangeInformation.tickSize = float(filter["tickSize"])
+                print(ExchangeInformation.tickSize)
+            if filter["filterType"] == "LOT_SIZE":
+                ExchangeInformation.stepSize = float(filter["stepSize"])
+
+        ExchangeInformation.balance = float(
+            Client.getAssetBalance(ExchangeInformation.quoteAsset)
+        ) * (IN_PLAY_PERCENT / 100)
